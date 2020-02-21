db11a18fa1dda2e135df6dd037cc05e368ab645b
Marius Havnaas <marhav95@gmail.com>
2020-02-21 12:27:28 +0100

    Ferdig stillt regex. Oblig 1 fullført.

--- src/main/Test/org/openjfx/CheckStringInputTest.java
+++ src/main/Test/org/openjfx/CheckStringInputTest.java
@@ -1,10 +1,52 @@
 package org.openjfx;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckStringInputTest {
+    @Test
+    void name() {
+        CheckStringInput.name("Marius Havnaas");
+        CheckStringInput.name("Marius Aleksander Østenby Havnaas");
+        CheckStringInput.name("Anne-Marte Moberg");
+        CheckStringInput.name("Ærling-Åse Østen");
+
+
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.name("Marius%¤/"));
+    }
+
+    @Test
+    void eMail() {
+    }
 
     @org.junit.jupiter.api.Test
+    void phoneNr() {
+        CheckStringInput.phoneNr("12233212");
+        CheckStringInput.phoneNr("+4712233212");
+        CheckStringInput.phoneNr("+47 12233212");
+        CheckStringInput.phoneNr("(+47)12233212");
+        CheckStringInput.phoneNr("(+47) 12233212");
+        CheckStringInput.phoneNr("(+47) 12 23 32 12");
+        CheckStringInput.phoneNr("07911 123456");
+        CheckStringInput.phoneNr("+44 7911 123456");
+        CheckStringInput.phoneNr("754-3010");
+        CheckStringInput.phoneNr("(541) 754-3010");
+        CheckStringInput.phoneNr("+1-541-754-3010");
+        CheckStringInput.phoneNr("1-541-754-3010");
+        CheckStringInput.phoneNr("001-541-754-3010");
+
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr(""));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("Not a number"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("-231"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("123-norway"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("1-541-æøå-3010"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("1-541-abc-3010"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("!%&/"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("123 123     123 12"));
+    }
+
+    /*@org.junit.jupiter.api.Test
     void navn() {
         CheckStringInput.name("Marius");
 
@@ -13,16 +55,36 @@ class CheckStringInputTest {
     }
 
     @org.junit.jupiter.api.Test
-    void ePost() {
+    void eMail() {
         CheckStringInput.eMail("abc@abc.abc");
+        CheckStringInput.eMail("abc@abc.ab.bc");
 
         assertThrows(InvalidStringException.class, () -> CheckStringInput.eMail("@abc.no"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.eMail("abc@.no"));
     }
 
     @org.junit.jupiter.api.Test
-    void telefonNr() {
-        CheckStringInput.phoneNr("12121212");
+    void phoneNr() {
+        CheckStringInput.phoneNr("12233212");
+        CheckStringInput.phoneNr("+4712233212");
+        //CheckStringInput.phoneNr("+47 12233212");
+        CheckStringInput.phoneNr("(+47)12233212");
+        CheckStringInput.phoneNr("(+47) 12233212");
+        CheckStringInput.phoneNr("(+47) 12 23 32 12");
+        CheckStringInput.phoneNr("07911 123456");
+        CheckStringInput.phoneNr("+44 7911 123456");
+        CheckStringInput.phoneNr("754-3010");
+        CheckStringInput.phoneNr("(541) 754-3010");
+        CheckStringInput.phoneNr("+1-541-754-3010");
+        CheckStringInput.phoneNr("1-541-754-3010");
+        CheckStringInput.phoneNr("001-541-754-3010");
 
-        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("345dv234"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr(""));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("Not a number"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("-231"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("123-norway"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("1-541-æøå-3010"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("1-541-abc-3010"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("!%&/"));
+        assertThrows(InvalidStringException.class, () -> CheckStringInput.phoneNr("123 123     123 12"));*/
     }
-}
--- src/main/java/org/openjfx/CheckStringInput.java
+++ src/main/java/org/openjfx/CheckStringInput.java
@@ -3,7 +3,7 @@ package org.openjfx;
 public class CheckStringInput {
 
     //Angir regexkode for navn.
-    private final static String NAME_PATTERN = "[A-ZÆØÅ][-a-zæøå]*";
+    private final static String NAME_PATTERN = "[A-ZÆØÅ][a-zæøå]+[ -]([A-ZÆØÅ][a-zæøå]+)?[ ]?([A-ZÆØÅ][a-zæøå]+)?[ ]?([A-ZÆØÅ][a-zæøå]+)?";
     //Angir regexkode for mail.
     //Regexkode hentet fra https://emailregex.com/
     private final static String EMAIL_PATTERN = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"" +
@@ -12,7 +12,7 @@ public class CheckStringInput {
             "?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-" +
             "\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     //Angir regexkode for telefon.
-    private final static String PHONENR_PATTERN = /*"[\\+]{1}?[0-9]{2}?*/"[0-9]{8}";
+    private final static String PHONENR_PATTERN = "[^-]([(]?[+]?[0-9]{1,3}[)]?)?( )?([- ])?[0-9]+([- ][0-9]*)?([- ][0-9]*)?([- ][0-9]*)?([- ][0-9]*)?";
 
 
     public static String name(String name){
